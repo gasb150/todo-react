0f64f8b9a020f20c9d039f2e8b08b4af8271ace2
tavo <gasb_150@hotmail.com>
2021-04-17 18:53:17 -0500

    Add project file and task file

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Projects from './components/project'
+function App() {
+  return (
+    <section className="App">
+      <Projects />
+    </section>
+  );
+}
+
+export default App;
